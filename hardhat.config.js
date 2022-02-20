cdf4860ca416b7ed37332cbb6b8d2aadc18af101
Wright <calebogundiya@gmail.com>
2022-02-20 09:29:16 +0100

    Setup env and hardhat config for fork

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,22 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+
+require("dotenv").config();
+
+const alchemyUrl = process.env.ALCHEMY_URL;
+
+
 module.exports = {
-  solidity: "0.7.3",
+  solidity: { version: "0.8.8" },
+  networks: {
+    hardhat: {
+      forking: {
+        url: alchemyUrl,
+        blockNumber: 4043801
+      }
+    }
+  }
 };
